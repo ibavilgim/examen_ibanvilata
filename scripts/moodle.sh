0d0ed17ecc9d65393d1bb061fe4ea2546c208af1
ibavilgim <ibavilgim@alu.edu.gva.es>
2024-12-04 20:36:16 +0100

    Update moodle.sh

--- scripts/moodle.sh
+++ scripts/moodle.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cd /var/www
 git clone -b MOODLE_405_STABLE git://git.moodle.org/moodle.git
 mkdir moodledata
@@ -9,17 +10,15 @@ sudo apt install php-curl -y
 sudo apt install php-zip -y
 sudo apt install php-gd -y
 sudo apt install php-intl -y
-
-sudo apt install php-zts
-sudo apt install php-soap
-sudo apt install php-xmlrp
-sudo apt install php-mbstring
-sudo apt install php-mysql
-sudo apt install php-mcrypt
-sudo apt install php-memcache
-sudo apt install php-memcached
+sudo apt install php-soap -y
+sudo apt install php-mbstring -y
+sudo apt install php-mysql -y
+sudo apt install php-mcrypt -y
+sudo apt install php-memcache -y
+sudo apt install php-memcached -y
 
 echo "max_input_vars =  5000" | sudo tee -a /etc/php/8.3/cli/php.ini
+
 sudo -u www-data /usr/bin/php /var/www/moodle/admin/cli/install.php
 
 # Falta crear VirtualHost, tabla base de datos "moodle"
